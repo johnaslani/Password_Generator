512714aa50484e8b85f3de98a951f0d80c521ee5
johnaslani <aslani.john@gmail.com>
2022-07-05 22:46:48 -0700

    Removed Consol.logs

--- Assets/script.js
+++ Assets/script.js
@@ -5,12 +5,13 @@ function generatePassword() {
   var passlength = prompt(
     "Select your lenght of your password between 8 to 128 alphanumeric characters. Please select at least one type of Character in the following"
   );
-  passlength = parseInt(passlength);
-  // console.log(typeof passlength)
-  // console.log(passlength === NaN)
-  if (passlength === NaN) generatePassword();
-
-  // var passtype = prompt ("Select L, U, N, S for each of the following elements: lowercase, uppercase, numeric, and/or special characters" (L, U, N, S))
+  // passlength = parseInt(passlength);
+  if (Number.isNaN(passlength)) {
+    alert("Password length must be provided as a number");
+    return null;
+  }
+  // console.log(passlength);
+  
 
   var numberChars = "0123456789";
   var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -26,14 +27,20 @@ function generatePassword() {
   if (uppercase) allChars += upperChars;
   if (lowercase) allChars += lowerChars;
   if (number) allChars += numberChars;
-  if (!special && !uppercase && !lowercase && !number) generatePassword();
-
- PW = Array(passlength);
+  if (!special && !uppercase && !lowercase && !number) { alert("Password needs to select at least one type of character");
+  return null;};
+  // console.log(allChars.length);
+  // console.log("allChars", allChars);
+ var PW = Array(passlength);
+//  console.log("PW ====", PW)
  for (let i=0; i<passlength; i++){
-  const randomindex = Math.floor(Math.random * (allChars.length + 1));
+  const randomindex = Math.floor(Math.random() * allChars.length);
+  // console.log("randomindex====", randomindex)
   PW[i]= allChars[randomindex];
+  // console.log(PW[i]);
  }
- console.log(PW[i]);
+
+//  console.log(PW[i]);
   return PW.join("");
 }
 
@@ -41,39 +48,10 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // console.log("password===", password);
+  // console.log("passwordText====", passwordText);
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-//Below are some comments for myself for keeping records
-
-// alert(passwordText.value);
-// alert(generatePassword(password.lenght));
-
-// var allChars = numberChars + upperChars + lowerChars + specialChars;
-
-// var random = fucntion () {
-
-// }
-
-// for (let i = 0; i < password.type.length; i++) {
-//   if (password.type[i]===
-// }
-// var allChars
-
-// if (typeof passlength !== Number) {
-// // // //   passlength = prompt ("Please enter a number")
-// // // // }
